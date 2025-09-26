a90e8e5a18154e8bf67d4b3c00f781e52b0c16cb
collollll <hck2061@naver.com>
2025-09-26 16:40:17 +0900

    inter

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+$(function () {
+  // $(".main li").on("click", function () {
+  //   $(".main li").not(this).removeClass("on");
+  //   $(this).toggleClass("on");
+  //   // $(".main .up").removeClass("move");
+  //   $(".main li").not(this).find(".up").removeClass("move");
+  //   $(this).find(".up").toggleClass("move");
+  // });
+
+  $(".main li").on("click", function () {
+    let isOn = $(this).hasClass("on");
+    if (isOn) {
+      $(this).removeClass("on");
+      $(this).find(".up").removeClass("move");
+    } else {
+      $(".main li").removeClass("on");
+      $(this).addClass("on");
+      $(".main li").find(".up").removeClass("move");
+      $(this).find(".up").addClass("move");
+    }
+  });
+});
